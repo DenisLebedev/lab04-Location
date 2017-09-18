46ffebb26c753922124dd341f4585b02fac059c2
Denis Lebedev <01lebedev.denis@gmail.com>
2017-09-18 15:12:35 -0400

    Trying to solver and understand the problem

--- app/src/main/java/denislebedev/ca/lab04_location/MainActivity.java
+++ app/src/main/java/denislebedev/ca/lab04_location/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton imgBut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgBut = (ImageButton) findViewById(R.id.imgBut);
+        Uri country = Uri.parse("geo:45.489374,-73.588298?q=" +
+                Uri.encode(getResources().getString(R.string.country)));
+        playMedia(country);
     }
 
     public void playMedia(Uri file) {
